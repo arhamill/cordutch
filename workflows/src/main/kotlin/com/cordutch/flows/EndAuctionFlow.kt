ade439d68e2e85881b42f07b30d6e4937f6d9ac9
Angus Hamill <angushamill@gmail.com>
2019-09-18 18:07:37 +0100

    End auction flow

---
+++ workflows/src/main/kotlin/com/cordutch/flows/EndAuctionFlow.kt
@@ -0,0 +1,53 @@
+package com.cordutch.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import com.cordutch.contracts.AuctionContract
+import com.cordutch.contracts.AuctionableAssetContract
+import com.cordutch.states.AuctionState
+import com.cordutch.states.AuctionableAsset
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.StateAndContract
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.flows.*
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+@InitiatingFlow
+@StartableByRPC
+class EndAuctionFlow(val auctionId: UniqueIdentifier) : FlowLogic<SignedTransaction>() {
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val auctionCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(auctionId))
+        val auctionResults = serviceHub.vaultService.queryBy<AuctionState>(auctionCriteria).states
+        if (auctionResults.size != 1) throw IllegalArgumentException("Auction id does not uniquely refer to an existing auction")
+        val auction = auctionResults.single()
+        val auctionState = auction.state.data
+
+        val assetCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(auctionState.asset.linearId))
+        val asset = serviceHub.vaultService.queryBy<AuctionableAsset>(assetCriteria).states.single()
+
+        val builder = TransactionBuilder(notary = serviceHub.networkMapCache.notaryIdentities.single())
+                .withItems(
+                        auction,
+                        Command(AuctionContract.Commands.End(), ourIdentity.owningKey),
+                        asset,
+                        Command(AuctionableAssetContract.Commands.Unlock(), ourIdentity.owningKey),
+                        StateAndContract(asset.state.data.unlock(), AuctionableAssetContract.ID)
+                )
+        val signedTx = serviceHub.signInitialTransaction(builder)
+        val otherSessions = (auctionState.participants - ourIdentity).map { initiateFlow(it) }
+        return subFlow(FinalityFlow(signedTx, otherSessions))
+    }
+}
+
+@InitiatedBy(EndAuctionFlow::class)
+class EndAuctionFlowResponder(val flowSession: FlowSession) : FlowLogic<Unit>() {
+
+    @Suspendable
+    override fun call() {
+        subFlow(ReceiveFinalityFlow(flowSession))
+    }
+}
---
+++ workflows/src/test/kotlin/com/cordutch/EndAuctionFlowTests.kt
@@ -0,0 +1,86 @@
+package com.cordutch
+
+import com.cordutch.flows.*
+import com.cordutch.states.AuctionState
+import com.cordutch.states.AuctionableAsset
+import net.corda.core.contracts.TransactionVerificationException
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.utilities.getOrThrow
+import net.corda.finance.POUNDS
+import net.corda.testing.internal.chooseIdentityAndCert
+import net.corda.testing.node.MockNetwork
+import net.corda.testing.node.MockNetworkNotarySpec
+import net.corda.testing.node.MockNodeParameters
+import net.corda.testing.node.StartedMockNode
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertFailsWith
+
+class EndAuctionFlowTests {
+    lateinit var mockNetwork: MockNetwork
+    lateinit var a: StartedMockNode
+    lateinit var b: StartedMockNode
+    lateinit var c: StartedMockNode
+
+    @Before
+    fun setup() {
+        mockNetwork = MockNetwork(listOf("com.cordutch"), notarySpecs = listOf(MockNetworkNotarySpec(CordaX500Name("Notary","London","GB"))))
+        a = mockNetwork.createNode(MockNodeParameters())
+        b = mockNetwork.createNode(MockNodeParameters())
+        c = mockNetwork.createNode(MockNodeParameters())
+        val startedNodes = arrayListOf(a, b, c)
+        // For real nodes this happens automatically, but we have to manually register the flow for tests
+        startedNodes.forEach {
+            it.registerInitiatedFlow(CreateAuctionResponderFlow::class.java)
+            it.registerInitiatedFlow(EndAuctionFlowResponder::class.java)
+        }
+        mockNetwork.runNetwork()
+    }
+
+    @After
+    fun tearDown() {
+        mockNetwork.stopNodes()
+    }
+
+    private fun StartedMockNode.createAuction() : SignedTransaction {
+        val assetFuture = this.startFlow(AssetIssueFlow("My asset"))
+        mockNetwork.runNetwork()
+        val asset = assetFuture.getOrThrow().tx.outputStates.single() as AuctionableAsset
+
+        val future = this.startFlow(CreateAuctionFlow(asset.linearId, 100.POUNDS, listOf(b, c).map { it.info.chooseIdentityAndCert().party }))
+        mockNetwork.runNetwork()
+        return future.getOrThrow()
+    }
+
+    @Test
+    fun flowReturnsCorrectlyFormedSignedTx() {
+        val auctionTx = a.createAuction().tx
+        val auction = auctionTx.outputsOfType<AuctionState>().single()
+        val future = a.startFlow(EndAuctionFlow(auction.linearId))
+        mockNetwork.runNetwork()
+        val stx = future.getOrThrow()
+
+        assert(auctionTx.outRef<AuctionState>(auction).ref in stx.inputs) {"Auction tx should be an input"}
+        assert(stx.tx.outputsOfType<AuctionableAsset>().single() == auction.asset.unlock()) { "Unlocked asset should be output" }
+        stx.verifyRequiredSignatures()
+    }
+
+    @Test
+    fun mustBeValidAuction() {
+        val future = a.startFlow(EndAuctionFlow(UniqueIdentifier()))
+        mockNetwork.runNetwork()
+        assertFailsWith<IllegalArgumentException> { future.getOrThrow() }
+    }
+
+    @Test
+    fun mustBeOwner() {
+        val auctionTx = a.createAuction().tx
+        val auction = auctionTx.outputsOfType<AuctionState>().single()
+        val future = b.startFlow(EndAuctionFlow(auction.linearId))
+        mockNetwork.runNetwork()
+        assertFailsWith<TransactionVerificationException> { future.getOrThrow() }
+    }
+}
