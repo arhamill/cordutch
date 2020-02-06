e90f97e9bbe3d4c2ab01adcaa00f14a4da8d6279
Angus Hamill <angushamill@gmail.com>
2020-02-06 09:28:20 +0000

    Use new version of cortex

--- frontend/src/context.js
+++ frontend/src/context.js
@@ -1,11 +1,11 @@
-import { createTokensContext, createStateContext } from '@arhamill/cortex'
+import { createTokensContext, createLinearContext } from '@arhamill/cortex'
 
 export const { tokensContext, TokensProvider } = createTokensContext('http://localhost:10055')
 
-const asset = createStateContext('http://localhost:10055', 'com.cordutch.states.AuctionableAsset')
-export const assetContext = asset.stateContext
-export const AssetProvider = asset.StateProvider
+const asset = createLinearContext('http://localhost:10055', 'com.cordutch.states.AuctionableAsset')
+export const assetContext = asset.linearContext
+export const AssetProvider = asset.LinearProvider
 
-const auction = createStateContext('http://localhost:10055', 'com.cordutch.states.AuctionState')
-export const auctionContext = auction.stateContext
-export const AuctionProvider = auction.StateProvider
+const auction = createLinearContext('http://localhost:10055', 'com.cordutch.states.AuctionState')
+export const auctionContext = auction.linearContext
+export const AuctionProvider = auction.LinearProvider
