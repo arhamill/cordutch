2382ff2436c0b33c500936f55c9757d54859435b
Angus Hamill <angushamill@gmail.com>
2020-02-06 17:14:37 +0000

    Update port

--- frontend/src/context.js
+++ frontend/src/context.js
@@ -1,11 +1,11 @@
 import { createTokensContext, createLinearContext } from '@arhamill/cortex'
 
-export const { tokensContext, TokensProvider } = createTokensContext('http://localhost:10055')
+export const { tokensContext, TokensProvider } = createTokensContext('http://localhost:8080')
 
-const asset = createLinearContext('http://localhost:10055', 'com.cordutch.states.AuctionableAsset')
+const asset = createLinearContext('http://localhost:8080', 'com.cordutch.states.AuctionableAsset')
 export const assetContext = asset.linearContext
 export const AssetProvider = asset.LinearProvider
 
-const auction = createLinearContext('http://localhost:10055', 'com.cordutch.states.AuctionState')
+const auction = createLinearContext('http://localhost:8080', 'com.cordutch.states.AuctionState')
 export const auctionContext = auction.linearContext
 export const AuctionProvider = auction.LinearProvider
